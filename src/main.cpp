3634c980fce21496e8d5936ba85da2f1cebd0907
niwantha33 <niwantha33@gmail.com>
2022-04-26 01:37:48 +0530

    ADD ENABLE.

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,13 @@ uint8_t output_pin = 6;
 
 uint8_t enable_pin = 7;
 
-int flagX = 0, flagY = 0;
+uint8_t flagX = 0, flagY = 0, flag_ex = 0, flag_ey = 0, out_y = 0, out_x = 0;
+uint8_t y[4] = {'\0'};
+uint8_t x[4] = {'\0'};
+
+uint8_t user_a = 12;
+uint8_t user_b = 13;
+uint8_t user_z = 14;
 
 void read_x()
 {
@@ -38,6 +44,8 @@ void setup()
 {
   pinMode(input_pin_x, INPUT_PULLDOWN);
   pinMode(input_pin_y, INPUT_PULLDOWN);
+  pinMode(output_pin, OUTPUT);
+  pinMode(enable_pin, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(input_pin_x), read_x, FALLING);
   attachInterrupt(digitalPinToInterrupt(input_pin_y), read_y, FALLING);
 }
@@ -48,18 +56,54 @@ void loop()
   if (flagX)
   {
     detachInterrupt(input_pin_x);
-    uint8_t x[4] ={'\0'}; 
-    for(i=4; i>0; i--)
+    memset(x, 0, sizeof(x));
+    for (i = 4; i > 0; i--)
     {
       x[i] = digitalRead(input_pin_x);
-      delayNanoseconds(100);
-
+      delayNanoseconds(100); /* Need to check with the input */
     }
     flagX = 0;
+    flag_ex = 1;
   }
-    if (flagY)
+  if (flagY)
   {
     detachInterrupt(input_pin_y);
+    memset(y, 0, sizeof(y));
+    for (i = 4; i > 0; i--)
+    {
+      y[i] = digitalRead(input_pin_y);
+      delayNanoseconds(100); /* Need to check with the input */
+    }
     flagY = 0;
+    flag_ey = 1;
+  }
+
+  if (flagX == 0 && flagY == 0 && flag_ex == 1 && flag_ey == 1)
+  {
+    out_y = y[0] << 3 | y[1] << 2 | y[2] << 1 | y[3] << 0;
+    out_x = x[0] << 3 | x[1] << 2 | x[2] << 1 | x[3] << 0;
+    if (user_a == out_x)
+    {
+
+      if (user_b == out_y)
+      {
+        digitalWrite(output_pin, LOW);
+        delayNanoseconds(108);
+        for (i = 0; i < 8; i++)
+        {
+          digitalWrite(output_pin, bitRead(user_z, i));
+          delayNanoseconds(108);
+        }
+        digitalWrite(output_pin, HIGH);
+        digitalWrite(enable_pin, HIGH);
+        delayNanoseconds(100);
+        digitalWrite(enable_pin, LOW);
+      }
+    }
+
+    flag_ex = 0;
+    flag_ey = 0;
+    attachInterrupt(digitalPinToInterrupt(input_pin_x), read_x, FALLING);
+    attachInterrupt(digitalPinToInterrupt(input_pin_y), read_y, FALLING);
   }
 }
